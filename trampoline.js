9a87979d4a0e6812e6dd1bc56cd74838496caae7
Dias, José (Developer) <jose.dias@sky.uk>
2018-12-04 11:29:45 +0000

    trampoline example works now

--- trampoline.js
+++ trampoline.js
@@ -1,50 +1,41 @@
-// FROM: https://stackoverflow.com/a/44244819/193711
-
-// a tag to uniquely identify thunks (zero-argument functions)
-const $thunk = Symbol.for('thunk');
-
-//  eagerly evaluate a lazy function until the final result
-const eager = f => (...args) => {
-  // console.log('eager', args);
-  let g = f(...args);
-  while (g && g[$thunk]) g = g();
-  return g;
-};
-
-// lift a normal binary function into the lazy context
-const lazy2 = f => (x, y) => {
-  // console.log('lazy2', x, y);
-  const thunk = () => f(x, y);
-  return (thunk[$thunk] = true), thunk;
-};
-
-// the stack-safe iterative function in recursive style
-const repeat = n => f => x => {
-  const aux = lazy2((n, x) => (n === 0 ? x : aux(n - 1, f(x))));
-  return eager(aux)(n, x);
-};
-
-const inc = x => x + 1;
-
-// and run...
-// console.log(repeat(1e6)(inc)(0)); // 1000000
-
-////////
-
 // FROM: http://raganwald.com/2013/03/28/trampolines-in-javascript.html
+// FROM: https://stackoverflow.com/a/44244819/193711 (also interesting)
 
-// NAIVE
+// 1ST FORM - naive implementation
 function _factorial(n) {
   return n ? n * factorial(n - 1) : 1;
 }
 
-// 2ND
-function factorial(n) {
+// 2ND - rewritten in tail-recursion form
+function __factorial(n) {
   function myself(acc, n) {
     return n ? myself(acc * n, n - 1) : acc;
   }
 
   return myself(1, n);
+}
+
+function trampoline(fun, ...args) {
+  let result = fun.apply(fun, args);
+
+  while (result instanceof Function) {
+    result = result();
+  }
+
+  return result;
+}
+
+// 3RD - trampolined: it now works!
+function factorial(n) {
+  function myself(acc, n) {
+    return n
+      ? function() {
+          return myself(acc * n, n - 1);
+        }
+      : acc;
+  }
+
+  return trampoline(myself, 1, n);
 }
 
 console.log('fact(10)', factorial(10)); //=> 3628800
