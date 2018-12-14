c0ecf7f7e90282cf109b221b5f16c6456ff9d932
José Pedro Dias <jose.pedro.dias@gmail.com>
2018-12-14 08:54:22 +0000

    13 2/3 q1

---
+++ 13.js
@@ -0,0 +1,128 @@
+const { Matrix } = require('./generic');
+
+const C0 = ' ';
+const CH = '-';
+const CV = '|';
+const CD19 = '/';
+const CD37 = '\\';
+const CINT = '+';
+const CCU = '^';
+const CCD = 'v';
+const CCL = '<';
+const CCR = '>';
+
+const R = 0;
+const U = 1;
+const L = 2;
+const D = 3;
+const IN_DIR = [CCR, CCU, CCL, CCD];
+const MOV = [[1, 0], [0, -1], [-1, 0], [0, 1]];
+
+/*
+    /->-\
+    |   |  /----\
+    | /-+--+-\  |
+    | | |  | v  |
+    \-+-/  \-+--/
+      \------/
+*/
+
+function parseInput(lines) {
+  function g(x, y) {
+    return lines[y][x];
+  }
+
+  const H = lines.length;
+  const W = lines[0].length;
+
+  const seeds = [];
+
+  const circuits = [];
+  const intersections = [];
+  const cars = [];
+
+  for (let y = 0; y < H; ++y) {
+    for (let x = 0; x < W; ++x) {
+      let c = g(x, y);
+
+      if (c === CCU || c === CCR || c === CCL || c === CCD) {
+        let dir = IN_DIR.indexOf(c);
+        cars.push([x, y, dir]);
+      }
+
+      if (c === CINT) {
+        intersections.push([x, y]);
+      }
+
+      if (c !== CD19 && c !== CCD && c !== CCR) {
+        continue;
+      }
+      c = g(x + 1, y);
+      if (c === C0 || c === CD19 || c === CD37) {
+        continue;
+      }
+      c = g(x, y + 1);
+      if (c === C0 || c === CD19 || c === CD37) {
+        continue;
+      }
+      seeds.push([x, y]);
+    }
+  }
+
+  // console.log('cars', cars);
+  // console.log('seeds', seeds);
+  seeds.forEach(([x0, y0]) => {
+    let x, y, c, x1, y1;
+    for (
+      x = x0;
+      (c = g(x, y0)), c !== C0 && c !== CD37 && c !== CCL && c !== CCD;
+      ++x
+    ) {}
+    x1 = x;
+    for (
+      y = y0;
+      (c = g(x0, y)), c !== C0 && c !== CD37 && c !== CCR && c !== CCU;
+      ++y
+    ) {}
+    y1 = y;
+    // console.log(`x0:${x0}, y0:${y0}, x1:${x1}, y1:${y1}`);
+    circuits.push([x0, y0, x1, y1]);
+  });
+
+  // console.log('circuits', circuits);
+
+  return { dimensions: [W, H], circuits, intersections, cars };
+}
+
+function printScreen({ dimensions: [W, H], circuits, intersections, cars }) {
+  const m = new Matrix(W, H, 0, 0, C0);
+
+  circuits.forEach(([x0, y0, x1, y1]) => {
+    m.setChar(x0, y0, CD19);
+    m.setChar(x0, y1, CD37);
+    m.setChar(x1, y0, CD37);
+    m.setChar(x1, y1, CD19);
+    for (let x = x0 + 1; x < x1; ++x) {
+      m.setChar(x, y0, CH);
+      m.setChar(x, y1, CH);
+    }
+    for (let y = y0 + 1; y < y1; ++y) {
+      m.setChar(x0, y, CV);
+      m.setChar(x1, y, CV);
+    }
+  });
+
+  intersections.forEach(([x, y]) => {
+    m.setChar(x, y, CINT);
+  });
+
+  cars.forEach(([x, y, dir]) => {
+    m.setChar(x, y, IN_DIR[dir]);
+  });
+
+  return m.toString();
+}
+
+function nextStep() {}
+
+module.exports = { parseInput, printScreen, nextStep };
--- 13.test.js
+++ 13.test.js
@@ -1,4 +1,4 @@
-const {} = require('./13');
+const { parseInput, printScreen, nextStep } = require('./13');
 const { fileAsLines } = require('./generic');
 
 const LINES = fileAsLines('./13.input.txt');
@@ -10,6 +10,24 @@ const EXAMPLE_LINES = `/->-\\
 \\-+-/  \\-+--/
   \\------/   `.split('\n');
 
-it('', () => {
-  console.log(EXAMPLE_LINES.join('\n'));
+it('parseInput', () => {
+  // console.log(EXAMPLE_LINES.join('\n'));
+  expect(parseInput(EXAMPLE_LINES)).toEqual({
+    dimensions: [13, 6],
+    circuits: [[0, 0, 4, 4], [7, 1, 12, 4], [2, 2, 9, 5]],
+    intersections: [[4, 2], [7, 2], [2, 4], [9, 4]],
+    cars: [[2, 0, 0], [9, 3, 3]]
+  });
 });
+
+it('printScreen', () => {
+  const st = {
+    dimensions: [13, 6],
+    circuits: [[0, 0, 4, 4], [7, 1, 12, 4], [2, 2, 9, 5]],
+    intersections: [[4, 2], [7, 2], [2, 4], [9, 4]],
+    cars: [[2, 0, 0], [9, 3, 3]]
+  };
+  expect(printScreen(st)).toEqual(EXAMPLE_LINES.join('\n'));
+});
+
+it('nextStep', () => {});
